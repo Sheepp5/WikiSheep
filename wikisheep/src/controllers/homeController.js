68167374b58143c819b327066a318dfa7cbd08fa
Juan Franco <JuanDFrancoM15@gmail.com>
2025-08-06 09:21:59 -0500

    end

---
+++ wikisheep/src/controllers/homeController.js
@@ -0,0 +1,13 @@
+// import { crearMain } from "../components/mainSection.js";
+
+// export function homeController() {
+//   const app = document.getElementById("app");
+
+//   while (app.firstChild) {
+//     app.removeChild(app.firstChild);
+//   }
+
+//   crearMain().then(content => {
+//     app.appendChild(content);
+//   });
+// }
